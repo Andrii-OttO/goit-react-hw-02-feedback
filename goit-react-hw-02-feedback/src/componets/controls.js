eac1c0e72778fee4d1b73e333ca2bd6fb5951598
Andrii-OttO <82340217+Andrii-OttO@users.noreply.github.com>
2021-12-08 20:07:02 +0300

    first attemption

---
+++ goit-react-hw-02-feedback/src/componets/controls.js
@@ -0,0 +1,20 @@
+import React from "react";
+const controls =({handleGoodClick,handleNeutralClick,handleBadClick})=>(
+    <div>
+            <h2>Please leave feedback</h2>
+            <button type="button" onClick={handleGoodClick}>Good</button>
+            <button type="button" onClick={handleNeutralClick}>Neutral</button>
+            <button type="button" onClick={handleBadClick}>Bad</button>
+            <h3>Statistics</h3>
+            
+   {/* <ul>
+      <li>Good:{this.state.good}</li>   
+      <li>Neutral:{this.state.neutral}</li>
+      <li>Bad:{this.state.bad}</li>
+      <li>Total:{}</li>
+      <li>Positive Feedback:{}%</li>
+      </ul> */}
+      </div>
+)
+
+    export default controls
---
+++ goit-react-hw-02-feedback/src/componets/statisctic.js
@@ -0,0 +1,24 @@
+//   import PropTypes from "prop-types";
+//   //import Feedback from "./feedback.js"
+//   const Statistics=({good,neutral,bad})=>{
+     
+   
+//    //   < Feedback
+//    // key={[button.value]}
+//    // />
+//    // <ul>
+//    //    <p>Good:{good}</p>   
+//    //    <li>Neutral:{neutral}</li>
+//    //    <li>bad:{bad}</li>
+//    //    <li>Total</li>
+//    //    <li>Positive Feedback:{}%</li>
+//    //    </ul>
+      
+//   }
+//   Statistics.protoTypes={
+//    good: PropTypes.number.isRequired,
+//    neutral: PropTypes.number.isRequired,
+//    bad: PropTypes.number.isRequired
+//   }
+
+//   export default Statistics
